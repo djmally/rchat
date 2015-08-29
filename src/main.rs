1094def61afd1bf72ea2397560c926f41c4e5b8a
David Mally <djmally@gmail.com>
2015-08-29 19:07:52 -0400

    Revert "Add AtomicMq trait and all of its weirdness"
    This reverts commit 52323e56f0704f39374a3ac87483aad5e9e31180.

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,11 @@ extern crate rustc_serialize;
 
 
 fn main() {
-    /*let mut listener = Actor::new("127.0.0.1", "8000");
+    let mut listener = Actor::new("127.0.0.1", "8000");
     let mut sender = Actor::new("127.0.0.1", "8001");
     let msg = message_processor::read_message();
     sender.enq_out_msg(&msg);
     sender.send_msg("127.0.0.1:8000");
     listener.recv_msg();
-    listener.dq_inc_msg();*/
-    println!("Fix me!");
+    listener.dq_inc_msg();
 }
